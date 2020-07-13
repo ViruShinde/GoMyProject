6d84118ca698d55c9992e258772f4c5aeed13ab8
Virendra <47695015+ViruShinde@users.noreply.github.com>
2020-07-13 19:19:00 +0530

    Deleted EmployeeRepository
    Added for testing purposes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.globeop.riskfeed.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.globeop.riskfeed.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
