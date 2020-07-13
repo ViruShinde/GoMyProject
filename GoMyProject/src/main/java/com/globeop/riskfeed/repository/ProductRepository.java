6c2955ff5265736d5ea328c98f4c03405eb3d671
Virendra <47695015+ViruShinde@users.noreply.github.com>
2020-07-13 19:19:19 +0530

    Deleted ProductRepository
    Added for testing purposes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.globeop.riskfeed.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.globeop.riskfeed.entity.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-}
