89ad41bd903189c512725050800ffd3af9aaf2da
vshinde <vshinde@sscinc.com>
2020-12-21 18:52:18 +0530

    Initial version.

---
+++ GoMyProject/src/main/java/com/globeop/riskfeed/dto/RestPageImpl.java
@@ -0,0 +1,44 @@
+package com.globeop.riskfeed.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class RestPageImpl extends PageImpl<SchedularDto> {
+
+	private static final long serialVersionUID = 1L;
+
+	@JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+	    public RestPageImpl(@JsonProperty("content") List<SchedularDto> content,
+	                        @JsonProperty("number") int number,
+	                        @JsonProperty("size") int size,
+	                        @JsonProperty("totalElements") Long totalElements,
+	                        @JsonProperty("pageable") JsonNode pageable,
+	                        @JsonProperty("last") boolean last,
+	                        @JsonProperty("totalPages") int totalPages,
+	                        @JsonProperty("sort") JsonNode sort,
+	                        @JsonProperty("first") boolean first,
+	                        @JsonProperty("numberOfElements") int numberOfElements) {
+
+	        super(content, PageRequest.of(number, size), totalElements);
+	    }
+
+	    public RestPageImpl(List<SchedularDto> content, Pageable pageable, long total) {
+	        super(content, pageable, total);
+	    }
+
+	    public RestPageImpl(List<SchedularDto> content) {
+	        super(content);
+	    }
+
+	    public RestPageImpl() {
+	        super(new ArrayList<>());
+	    }
+}
