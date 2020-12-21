4dec5f441d497670d590dc90cb71f1364eea482c
vshinde <vshinde@sscinc.com>
2020-12-21 19:50:26 +0530

    Added RestTemplate bean.

--- GoMyProject/src/main/java/com/globeop/riskfeed/Application.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
@@ -12,11 +14,19 @@ public class Application extends SpringBootServletInitializer{
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(Application.class);
 	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
+		
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("SpringApplication Started...");
+
 	}
+	
 	
 }
 
