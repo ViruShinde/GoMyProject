d435e5d4b68f9318a082b7a77ab725e408521916
vshinde <vshinde@sscinc.com>
2020-12-23 16:19:46 +0530

    Added some changes.

--- GoMyProject/src/main/java/com/globeop/riskfeed/service/ScheduledDetailsService.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/service/ScheduledDetailsService.java
@@ -56,12 +56,6 @@ public class ScheduledDetailsService implements CommonService<ScheduledDetails> 
 	}
 	
 	public Page<SchedularDto> findPaginated(String id, int pageNo, int pageSize, String sortField, String sortDirection, String urlpath) {
-		Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
-			Sort.by(sortField).descending();		
-		//Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-		//Page<ScheduledDetails> findPaginated = this.theScheduledDetailsRepository.findAll(pageable);
-		//System.out.println("@@@@@@@@@@@@@@@ "+id);
-		List<SchedularDto> l = new ArrayList<SchedularDto>();
 		String url="http://localhost:8082/schedular/getscheduledetails";
 		if(urlpath.contains("getscheduledetails")) {
 			url="http://localhost:8082/schedular/getscheduledetails";
@@ -69,35 +63,18 @@ public class ScheduledDetailsService implements CommonService<ScheduledDetails> 
 				url=url+"/"+id;
 			}
 		}else if(urlpath.contains("page")) {
-			url="http://localhost:8082/schedular/getscheduledetails/page/"+pageNo+"?sortField="+sortField+"&sortDir="+sortDirection;
+			url="http://localhost:8082/schedular/getscheduledetails/page/"+pageNo+"?sortField="+sortField+"&sortDir="+sortDirection+"&records="+pageSize;
 		}
 				
-		//System.out.println("URL "+url);
-		/*
-		ResponseEntity<SchedularDto[]> response = restTemplate.getForEntity(url,SchedularDto[].class);	
-		
-	    SchedularDto[] dto = response.getBody();
-	    for(SchedularDto sdto : dto) {
-	    	l.add(sdto);
-	    }
-	    */
-	    //Page<ScheduledDetails> findPaginated  =  restTemplate.getForObject(url,Page.class);
 		RestPageImpl findPaginated = restTemplate.getForObject(url, RestPageImpl.class);
-	    //System.out.println("findPaginated.toString() >>>> "+findPaginated.getContent().toString());
-	    //System.out.println(findPaginated.getTotalElements());
-	    
-	    //return new PageImpl<SchedularDto>(l);
 	    return findPaginated;
 	    
 	    
 			    
 	}
 	
-	public Page<SchedularDto> getscheduledetailsSearch(String keyword) {
-		
-		String url="http://localhost:8082/schedular/getscheduledetails/search/?keyword="+keyword;		
-		//Sort sort = "ASC".equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by("scheduledDetailsId").ascending():Sort.by("scheduledDetailsId").descending();		
-		//Pageable pageable = PageRequest.of(0, 5, sort);
+	public Page<SchedularDto> getscheduledetailsSearch(String keyword,int noOfRecords) {		
+		String url="http://localhost:8082/schedular/getscheduledetails/search/?keyword="+keyword+"&records="+noOfRecords;		
 		RestPageImpl findPaginated = restTemplate.getForObject(url, RestPageImpl.class);		
 		return findPaginated;
 	
