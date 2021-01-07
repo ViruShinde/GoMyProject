86a067723ae4d6acde5fd4e809a4b93c701d57cf
bisingh <bisingh@globeop.com>
2021-01-07 21:02:23 +0530

    Added Pageable methods

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/ClientRepository.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/repository/ClientRepository.java
@@ -1,10 +1,13 @@
 package com.globeop.riskfeed.repository;
 
+
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.globeop.riskfeed.dto.TestDto;
 import com.globeop.riskfeed.entity.ClientTable;
@@ -20,4 +23,15 @@ public interface ClientRepository extends JpaRepository<ClientTable, Integer> {
 	
 	@Query(value= "SELECT NEW com.globeop.riskfeed.dto.TestDto(c.clientID) from ClientTable AS c ")
 	public List<TestDto> testQuery();
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.ClientTable(c.clientID, c.clientShortName, c.modified_date) from ClientTable AS c ")
+	public Page<ClientTable> findAllPageable(Pageable pageable);
+
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.ClientTable(c.clientID, c.clientShortName, c.modified_date) from ClientTable AS c where c.clientID=?1 ")
+	public Page<ClientTable> findByIdPageable(Pageable pageable, int id);
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.ClientTable(c.clientID, c.clientShortName, c.modified_date) "
+			+ "from ClientTable AS c "
+			+ "where c.clientShortName LIKE %?1%")
+	public Page<ClientTable> searchClientPageable(Pageable pageable, String keyword);
 }
