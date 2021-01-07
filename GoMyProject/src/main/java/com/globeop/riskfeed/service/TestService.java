9e0301c9ad6532b8849988283b805e931c3f3aab
bisingh <bisingh@globeop.com>
2021-01-07 21:09:08 +0530

    Added pageable testing

--- GoMyProject/src/main/java/com/globeop/riskfeed/service/TestService.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/service/TestService.java
@@ -3,158 +3,63 @@ package com.globeop.riskfeed.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+import com.globeop.riskfeed.dto.RestPageImpl;
 import com.globeop.riskfeed.entity.ClientTable;
 
-public class TestService implements JpaRepository<ClientTable, Integer> {
-
-	@Override
-	public Page<ClientTable> findAll(Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Optional<ClientTable> findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean existsById(Integer id) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public long count() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+@Service
+public class TestService{
+	@Autowired
+	ClientService theClientService;
+	
+	@Autowired
+	FundService theFundService;
+	
+	private Sort sort;
+	private Pageable pageable;
+	private PageableService thePageableService;
+	
+	public Page getDetails(String id, String sortField, String requestFor) {
+		//Sort sort = "asc".equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();			
+		//Pageable pageable = PageRequest.of(0, 10, sort);
+		//PageableService thePageableService=getService(requestFor);
+		getServiceConfig(requestFor, sortField, 0, 10);
+		if(null == id || "".equals(id)) {
+			return thePageableService.findAllPage(pageable);
+		}else {
+			return thePageableService.findByIdPage(pageable,Integer.parseInt(id));
+		}
+		
+		
+			//Page<ClientTable> page=new RestPageImpl(theClientService.findAll(), pageable, 10);
+			
+		
 		
 	}
-
-	@Override
-	public void delete(ClientTable entity) {
-		// TODO Auto-generated method stub
+	
+	public void getServiceConfig(String requestFor, String sortField, int startPage, int recordSize) {		
+		if(requestFor.equals("client")) {
+			thePageableService=theClientService;
+			
+		}else if(requestFor.equals("fund")) {
+			thePageableService=theFundService;
+		}
+		sort = "asc".equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+		pageable = PageRequest.of(startPage, recordSize, sort);
 		
 	}
-
-	@Override
-	public void deleteAll(Iterable<? extends ClientTable> entities) {
-		// TODO Auto-generated method stub
+	
+	public Page getSearchDetails(String keyword, int record , String requestFor, String sortField) {
+		getServiceConfig(requestFor, sortField, 0, record);
 		
+		return thePageableService.getSearchDetails(pageable, keyword);
 	}
-
-	@Override
-	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <S extends ClientTable> Optional<S> findOne(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> Page<S> findAll(Example<S> example, Pageable pageable) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> long count(Example<S> example) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public <S extends ClientTable> boolean exists(Example<S> example) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public List<ClientTable> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ClientTable> findAll(Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<ClientTable> findAllById(Iterable<Integer> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> List<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void flush() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public <S extends ClientTable> S saveAndFlush(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void deleteInBatch(Iterable<ClientTable> entities) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void deleteAllInBatch() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public ClientTable getOne(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> List<S> findAll(Example<S> example) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public <S extends ClientTable> List<S> findAll(Example<S> example, Sort sort) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
