39fc8ab74a5b08263952b23b8be2e15a5a51eafc
bisingh <bisingh@globeop.com>
2021-01-07 21:02:55 +0530

    Added pageable methods

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/FundTableRepository.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/repository/FundTableRepository.java
@@ -2,7 +2,10 @@ package com.globeop.riskfeed.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.globeop.riskfeed.entity.ClientTable;
 import com.globeop.riskfeed.entity.FundTable;
@@ -13,4 +16,14 @@ public interface FundTableRepository extends JpaRepository<FundTable, Integer> {
 	public List<FundTable> findByClient(ClientTable theClientTable);
 	
 	public List<FundTable> findByFundShortName(String fundShortName);
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.FundTable(f.fundID,f.fundShortName,f.modified_date) from FundTable AS f ")
+	public Page<FundTable> findAllPageable(Pageable pageable);
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.FundTable(f.fundID,f.fundShortName,f.modified_date) from FundTable AS f where f.fundID=?1 ")
+	public Page<FundTable> findByIdPageable(Pageable pageable, int id);
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.FundTable(f.fundID,f.fundShortName,f.modified_date) "
+			+ "from FundTable AS f where f.fundShortName LIKE %?1% ")
+	public Page<FundTable> searchFundPageable(Pageable pageable, String keyword);
 }
