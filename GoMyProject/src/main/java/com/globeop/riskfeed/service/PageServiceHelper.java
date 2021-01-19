e2a3f1e7a138b1112c5447512e96cd28256a9ced
bisingh <bisingh@globeop.com>
2021-01-19 14:33:54 +0530

    Rearranged arguments

--- GoMyProject/src/main/java/com/globeop/riskfeed/service/PageServiceHelper.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/service/PageServiceHelper.java
@@ -24,25 +24,31 @@ public class PageServiceHelper {
 	private Pageable pageable;
 	private PageableService thePageableService;
 	
-	public Page getDetails(String id, String sortField, String requestFor, int pageNo, int recordSize) {
+	public Page getDetails(String requestFor,String id, int pageNo,String sortField,String sortDir,String keyword, int recordSize) {
 		//Sort sort = "asc".equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();			
 		//Pageable pageable = PageRequest.of(0, 10, sort);
 		//PageableService thePageableService=getService(requestFor);
-		getServiceConfig(requestFor, sortField, pageNo, recordSize);
-		if(null == id || "".equals(id)) {
-			return thePageableService.findAllPage(pageable);
+		getServiceConfig(requestFor, sortField, pageNo, recordSize,sortDir);
+		Page page=null;
+		if(keyword == null || "".equals(keyword)) {
+			if(null == id || "".equals(id)) {
+				page= thePageableService.findAllPage(pageable);
+			}else {
+				page= thePageableService.findByIdPage(pageable,Integer.parseInt(id));
+			}
 		}else {
-			return thePageableService.findByIdPage(pageable,Integer.parseInt(id));
-		}
-		
-		
-			//Page<ClientTable> page=new RestPageImpl(theClientService.findAll(), pageable, 10);
+			if(null == id || "".equals(id)) {
+				page=thePageableService.getSearchDetails(pageable, keyword);
+			}else {
+				page=thePageableService.getSearchDetails(pageable, keyword,Integer.parseInt(id));
+			}
+		}								
 			
-		
+		return page;
 		
 	}
 	
- public String commonMethod(Page page, Model model, int pageNo, int pageSize, String sortField, String sortDir, String pageName) {
+ public String commonMethod(String returnPage,String id, int pageNo,String sortField,String sortDir,String keyword, int recordSize, Page page, Model model) {
 		 
 		 List details = page.getContent();			
 			model.addAttribute("currentPage", pageNo);
@@ -50,27 +56,35 @@ public class PageServiceHelper {
 			model.addAttribute("totalItems", page.getTotalElements());			
 			model.addAttribute("sortField", sortField);
 			model.addAttribute("sortDir", sortDir);
-			model.addAttribute("records", pageSize);
+			model.addAttribute("records", recordSize);
 			model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");			
 			model.addAttribute("details", details);
-			return pageName;
+			model.addAttribute("id", id);
+			model.addAttribute("keyword",keyword);			
+			/*System.out.println("pageNo ="+pageNo+" totalPages="+page.getTotalPages()+" totalItems="+page.getTotalElements()+
+					" sortField="+sortField+" sortDir="+sortDir+" records="+recordSize+" details="+details+" id="+id);*/
+			return returnPage;
 	 }
 	
 	
-	public void getServiceConfig(String requestFor, String sortField, int startPage, int recordSize) {		
+	public void getServiceConfig(String requestFor, String sortField, int startPage, int recordSize,String sortDir) {		
 		if(requestFor.equals("client")) {
 			thePageableService=theClientService;
 			
 		}else if(requestFor.equals("fund")) {
 			thePageableService=theFundService;
 		}
-		sort = "asc".equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
+		if(startPage !=0) {
+			startPage=startPage-1;
+		}
+		
+		sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() : Sort.by(sortField).descending();
 		pageable = PageRequest.of(startPage, recordSize, sort);
 		
 	}
 	
-	public Page getSearchDetails(String keyword, int record , String requestFor, String sortField) {
-		getServiceConfig(requestFor, sortField, 0, record);
+	public Page getSearchDetails(String keyword, int record , String requestFor, String sortField,String sortDir) {
+		getServiceConfig(requestFor, sortField, 0, record,sortDir);
 		
 		return thePageableService.getSearchDetails(pageable, keyword);
 	}
