89865468cddc90c77df00931e107e14a39f1445b
bisingh <bisingh@globeop.com>
2021-02-04 18:34:39 +0530

    Added query for pagination

--- GoMyProject/src/main/java/com/globeop/riskfeed/repository/DevelopmentRepository.java
+++ GoMyProject/src/main/java/com/globeop/riskfeed/repository/DevelopmentRepository.java
@@ -1,13 +1,46 @@
 package com.globeop.riskfeed.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.globeop.riskfeed.entity.ClientTable;
 import com.globeop.riskfeed.entity.DevelopmentTable;
+import com.globeop.riskfeed.entity.FtpServerDetails;
 import com.globeop.riskfeed.entity.RiskAggregator;
+import com.globeop.riskfeed.enums.IsWaivedOff;
 
 public interface DevelopmentRepository extends JpaRepository<DevelopmentTable, Integer> {
 
 	public List<DevelopmentTable> findByClientAndRiskAggregator(ClientTable theClientTable, RiskAggregator theAggregator);
+	
+	
+
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.DevelopmentTable(d.developmentId, d.developmentHours, d.developmentCost, d.isWaivedOff, "
+			+ "	d.startDate, d.endDate, d.developmentComments, d.approvalMail, d.fileName, "
+			+ "	d.fileType, c,r, d.modified_date ) "			
+			+ " from DevelopmentTable AS d"
+			+ " LEFT JOIN ClientTable AS c " 
+			+ " on c.clientID = d.client.clientID "
+			+ " LEFT JOIN RiskAggregator AS r " 
+			+ " on r.riskAggregatorId = d.riskAggregator.riskAggregatorId ")
+	public Page<DevelopmentTable> findAllPageable(Pageable pageable);
+
+	
+	@Query(value= "SELECT NEW com.globeop.riskfeed.entity.DevelopmentTable(d.developmentId, d.developmentHours, d.developmentCost, d.isWaivedOff, "
+			+ "	d.startDate, d.endDate, d.developmentComments, d.approvalMail, d.fileName, "
+			+ "	d.fileType, c,r, d.modified_date ) "			
+			+ " from DevelopmentTable AS d"
+			+ " LEFT JOIN ClientTable AS c " 
+			+ " on c.clientID = d.client.clientID "
+			+ " LEFT JOIN RiskAggregator AS r " 
+			+ " on r.riskAggregatorId = d.riskAggregator.riskAggregatorId "
+			+ " where d.developmentHours LIKE %?1% OR d.developmentCost LIKE %?1% OR d.isWaivedOff LIKE %?1% OR "
+			+ " d.startDate LIKE %?1% OR d.endDate LIKE %?1% OR d.developmentComments LIKE %?1% OR "
+			+ " c.clientShortName LIKE %?1% OR r.riskAggregatorName LIKE %?1% ")	
+	public Page<DevelopmentTable> searchPageable(Pageable pageable, String keyword);
 }
